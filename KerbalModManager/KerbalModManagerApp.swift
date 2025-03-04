35d50a7615551cc58b4a24d69ff382259b52dc1b
Lewis <mcclelll@icloud.com>
2025-03-03 17:13:21 -0800

    Initial Commit

---
+++ KerbalModManager/KerbalModManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KerbalModManagerApp.swift
+//  KerbalModManager
+//
+//  Created by Lewis McClelland on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct KerbalModManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KerbalModManagerTests/KerbalModManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  KerbalModManagerTests.swift
+//  KerbalModManagerTests
+//
+//  Created by Lewis McClelland on 3/3/25.
+//
+
+import Testing
+@testable import KerbalModManager
+
+struct KerbalModManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
