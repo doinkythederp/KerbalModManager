522c193b39596a420f20de651bd23248559a51ce
Lewis <mcclelll@icloud.com>
2025-03-24 16:50:24 -0700

    refactor: split out mod browser toolbar

--- KerbalModManager/Mod Browser/ModBrowser.swift
+++ KerbalModManager/Mod Browser/ModBrowser.swift
@@ -10,46 +10,46 @@ import CkanAPI
 
 struct ModBrowser: View {
     var instance: GameInstance
-
-    @State private var isRenamingInstance = false
-    @State private var editedInstanceName = ""
-
-    @Environment(\.dismiss) private var dismiss
-    @Environment(Store.self) private var store: Store?
+//    var registry: GameRegistry
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello world")
             .padding()
             .navigationTitle("Mod Browser")
             .navigationSubtitle(instance.name)
             .focusedSceneValue(\.selectedGameInstance, instance)
             .toolbar {
-//                ToolbarItemGroup(placement: .navigation) {
-//                    Button("Select Instance", systemSymbol: .chevronBackward) {
-//                        dismiss()
-//                    }
-//                    .help("Choose another game instance")
-//                }
+                ModBrowserToolbar(instance: instance)
+            }
+    }
+}
 
-                ToolbarItemGroup(placement: .navigation) {
-                    Button("Rename Instance", systemSymbol: .pencilLine) {
-                        store?.instanceBeingRenamed = instance
-                    }
-                    .help("Rename this game instance")
-                    .popover(isPresented: $isRenamingInstance, arrowEdge: Edge.bottom) {
-                        Form {
-                            TextField("Instance Name:", text: $editedInstanceName)
-                                .onAppear {
-                                    editedInstanceName = instance.name
-                                }
-                                .onSubmit {
-                                    instance.rename(editedInstanceName)
-                                }
-                                .padding()
+private struct ModBrowserToolbar: ToolbarContent {
+    var instance: GameInstance
+
+    @State private var isRenamingInstance = false
+    @State private var editedInstanceName = ""
+
+    @Environment(Store.self) private var store: Store?
+
+    var body: some ToolbarContent {
+        ToolbarItemGroup(placement: .navigation) {
+            Button("Rename Instance", systemSymbol: .pencilLine) {
+                store?.instanceBeingRenamed = instance
+            }
+            .help("Rename this game instance")
+            .popover(isPresented: $isRenamingInstance, arrowEdge: Edge.bottom) {
+                Form {
+                    TextField("Instance Name:", text: $editedInstanceName)
+                        .onAppear {
+                            editedInstanceName = instance.name
                         }
-                        .frame(width: 400)
-                    }
+                        .onSubmit {
+                            instance.rename(editedInstanceName)
+                        }
+                        .padding()
                 }
+                .frame(width: 400)
             }
             .onChange(of: store?.instanceBeingRenamed) {
                 isRenamingInstance = store?.instanceBeingRenamed == instance
@@ -63,6 +63,7 @@ struct ModBrowser: View {
                     }
                 }
             }
+        }
     }
 }
 
